06326c1730d19140ec69d413bda5046927137363
Felipe Almeida <felipegustavotheboss@gmail.com>
2023-10-04 18:01:47 -0300

    Adição do botão de Páginação

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,4 @@
 function pokemonCard(pokemon) {
-	console.log(pokemon.types);
-
 	const colours = {
 		normal: '#A8A77A',
 		fire: '#EE8130',
@@ -23,39 +21,44 @@ function pokemonCard(pokemon) {
 	};
 
 	return `
-        <li class="pokemon" style="background-color: ${
-			colours[pokemon.type]
-		};" onmouseover="this.style.boxShadow='${
-		colours[pokemon.type]
-	} 0px 8px 24px'" onmouseout="this.style.boxShadow='none'"">
-            <span class="number">#${pokemon.number
-				.toString()
-				.padStart(3, '0')}</span>
-            <span class="name">${pokemon.name}</span>
+	<li class="pokemon" style="background-color: ${colours[pokemon.type]}"
+    onmouseover="this.style.boxShadow='${colours[pokemon.type]} 0px 8px 24px'"
+    onmouseout="this.style.boxShadow='none'">
+    	<span class="number">#${pokemon.number.toString().padStart(3, '0')}</span>
+    	<span class="name">${pokemon.name}</span>
 
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types
-						.map(
-							(type) =>
-								`<li class="type" style="background-color: ${
-									colours[pokemon.type]
-								};">${type}</li>`
-						)
-						.join('')}
-                </ol>
-                <img
-                    src="${pokemon.picture}"
-                    alt="${pokemon.name}"/>
-            </div>
-        </li>`;
+    	<div class="detail">
+        	<ol class="types">
+            	${pokemon.types
+					.map(
+						(type) =>
+							`<li class="type" style="background-color: ${colours[type]};">${type}<li>`
+					)
+					.join('')}
+        	</ol>
+        	<img src="${pokemon.picture}" alt="${pokemon.name}"
+			style="filter: drop-shadow(5px 5px rgba(0, 0, 0, 0.2));"/>
+    	</div>
+</li>`;
 }
 
 const pokemonHTML = document.getElementById('pokemons');
+const loadMoreButton = document.getElementById('loadMore');
+let offset = 0;
+const limit = 12;
 
-pokeApi
-	.getPokemons()
-	.then((pokemons) => {
-		pokemonHTML.innerHTML += pokemons.map(pokemonCard).join('');
-	})
-	.catch((error) => console.log(error));
+function loadPokemons(offset, limit) {
+	pokeApi
+		.getPokemons(offset, limit)
+		.then((pokemons) => {
+			pokemonHTML.innerHTML += pokemons.map(pokemonCard).join('');
+		})
+		.catch((error) => console.log(error));
+}
+
+loadPokemons(offset, limit);
+
+loadMoreButton.addEventListener('click', () => {
+	offset += limit;
+	loadPokemons(offset, limit);
+});
